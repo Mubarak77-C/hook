96314ca79a8171d30ff4ab88085c9d8ecff91b3e
Mubarak <mubaraksache77@gmail.com>
2020-11-13 02:12:52 +0500

    Updates

---
+++ precache-manifest.b57cdb3190450cab8cf0fba5981e0e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3f3d82856ae86c70e16f1357dfc2f5",
+    "url": "/hook/index.html"
+  },
+  {
+    "revision": "4c66a6fde2bdd8585fc7",
+    "url": "/hook/static/css/main.d0b45d3a.chunk.css"
+  },
+  {
+    "revision": "3ff51cb66390472ecf03",
+    "url": "/hook/static/js/2.6af57190.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/hook/static/js/2.6af57190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c66a6fde2bdd8585fc7",
+    "url": "/hook/static/js/main.9a5b0a0b.chunk.js"
+  },
+  {
+    "revision": "682ec33abd363ea55964",
+    "url": "/hook/static/js/runtime-main.c7e000f6.js"
+  }
+]);
